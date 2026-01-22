dcd15d3603c5c897504629603c1194818806e42f
Anabel Trujillo Rodriguez <anabeltrujillorodriguez@gmail.com>
2026-01-21 19:46:40 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-d994a905-c3f3-4229-9dde-331f3aac38f2.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
